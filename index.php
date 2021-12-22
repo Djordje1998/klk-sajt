8030840f4fd67fffd76e8c895efb87d6887531fd
Djordje1999 <djordjen7@gmail.com>
2021-12-22 20:51:53 +0100

    Izmenjeno kako bi radile dodate operacije

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $uname = $_POST['username'];
     $upass = $_POST['password'];
 
-    $korisnik = new User(null, $uname, $upass);
+    $korisnik = new User(1, $uname, $upass);
     $odg = User::logInUser($korisnik, $conn);
 
     if ($odg->num_rows==1) {
