206fb11f90064695d80ba7e6f3d15d4225d6c2ec
Djordje1999 <djordjen7@gmail.com>
2021-12-22 21:31:25 +0100

    ispravke

--- handler/update.php
+++ handler/update.php
@@ -3,9 +3,9 @@
 require "../dbBroker.php";
 require "../model/prijava.php";
 
-if (isset($_POST['id'])&&isset($_POST['predmet']) && isset($_POST['katedra']) && isset($_POST['sala']) && isset($_POST['datum'])) {
-    $prijava = new Prijava($_POST['id'], $_POST['predmet'], $_POST['katedra'], $_POST['sala'], $_POST['datum']);
-    $status = $prijava->update($prijava, $conn);
+if (isset($_POST['predmet']) && isset($_POST['katedra']) && isset($_POST['sala']) && isset($_POST['datum'])) {
+    $prijava = new Prijava(null, $_POST['predmet'], $_POST['katedra'], $_POST['sala'], $_POST['datum']);
+    $status = $prijava->update($_POST['id'], $conn);
     if ($status) {
         echo "Success";
     } else {
